90e80b26c7033e67de394915343f90e7ca447db4
gbhopal <gbhopal91@gmail.com>
2025-12-24 12:38:37 +0000

    Refactor ParseDate function for clarity and consistency in variable naming

--- utils/utils.go
+++ utils/utils.go
@@ -8,12 +8,12 @@ import (
 )
 
 func ParseDate(date string) (*time.Time, error) {
-	_parseDate, err := time.Parse(time.RFC3339, date)
+	pd, err := time.Parse(time.RFC3339, date)
 	if err != nil {
 		return nil, err
 	}
 
-	return &_parseDate, nil
+	return &pd, nil
 }
 
 func PrintSessionStatus(s *domain.Session, now time.Time) {
